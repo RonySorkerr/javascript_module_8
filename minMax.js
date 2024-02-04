8ba02e0ad42c60fb3e68cb19605edcff867ed517
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-04 11:10:38 +0600

    min max

--- minMax.js
+++ minMax.js
@@ -1,9 +1,12 @@
-const numbers = [1, 8, 8, 5, 8, 5, 89, 5, 8];
-const nums = [];
-for(number of numbers){
-    console.log(number)
-}
-const max = Math.max(nums);
-console.log('max is : ' , max);
-// const min = Math.min(5, 85, 8, 5, 8, 2, 8, 5);
-// console.log(min);
+// const numbers = [1, 8, 8, 5, 8, 5, 89, 5, 8];
+// const nums = [];
+// for(number of numbers){
+//     console.log(number)
+// }
+// const max = Math.max(nums);
+// console.log('max is : ' , max);
+const min = Math.min(5, 85, 8, 5, 8, 2, 8, 5);
+console.log(min);
+
+const max = Math.max(4,5,8,56,9,3,5,2,58);
+console.log(max)
