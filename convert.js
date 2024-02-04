c88319f098f5a98922364e4a6c9fa8c912e1f3cd
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-04 09:16:03 +0600

    inch to feet

---
+++ convert.js
@@ -0,0 +1,18 @@
+// converting inch to feet
+
+function inchToFeet(inch){
+    const feet = inch / 12;
+    return feet;
+}
+
+const feet = inchToFeet(75);
+// console.log(feet);
+
+
+function inchToFeet2(inch){
+    const feet = inch / 12;
+    const feetNumber = parseInt(feet);
+}
+
+const myHeight = inchToFeet2(78);
+console.log(inchToFeet2);
